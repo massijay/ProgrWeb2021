19685d4d5c57b6174e8536899cb7ddc9e8849cef
Massimiliano Cristarella <m.cristarella@outlook.com>
2022-09-06 00:33:59 +0200

    Added necessary methods to query DB in DBProvider class

--- src/main/java/com/mcris/triprecorder/providers/DBProvider.java
+++ src/main/java/com/mcris/triprecorder/providers/DBProvider.java
@@ -1,12 +1,15 @@
 package com.mcris.triprecorder.providers;
 
+import com.mcris.triprecorder.models.entities.Geopoint;
 import com.mcris.triprecorder.models.entities.Session;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.Persistence;
+import com.mcris.triprecorder.models.entities.Trip;
+import com.mcris.triprecorder.models.entities.User;
+import org.hibernate.Hibernate;
 
+import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 public class DBProvider {
@@ -32,11 +35,180 @@ public class DBProvider {
     public Session getSession(String sessionToken) {
         EntityManager em = getNewNetityManager();
         try {
-            return em.find(Session.class, UUID.fromString(sessionToken));
-        }catch(Exception ex){
+            Session s = em.find(Session.class, UUID.fromString(sessionToken));
+            Hibernate.initialize(s);
+            return s;
+        } catch (Exception ex) {
             throw new RuntimeException(ex);
         } finally {
             em.close();
         }
+    }
+
+    public Session createNewSession(User user) {
+        if (user != null) {
+            EntityManager em = getNewNetityManager();
+            try {
+                Session session = new Session();
+                session.setUserId(user.getId());
+                session.setExpireAt(Timestamp.valueOf("2030-04-04 23:59:59"));
+                em.getTransaction().begin();
+                em.persist(session);
+                em.getTransaction().commit();
+                return session;
+            } catch (NoResultException ex) {
+                return null;
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            } finally {
+                em.close();
+            }
+        }
+        return null;
+    }
+
+    public boolean deleteSession(UUID sessionToken) {
+        if (sessionToken != null) {
+            EntityManager em = getNewNetityManager();
+            try {
+                em.getTransaction().begin();
+                Query query = em.createNamedQuery("Session.deleteByToken");
+                query.setParameter("tokenString", sessionToken);
+                int result = query.executeUpdate();
+                em.getTransaction().commit();
+                return result == 1;
+            } catch (Exception ex) {
+                return false;
+            } finally {
+                em.close();
+            }
+        }
+        return false;
+    }
+
+    public User getUserbyUsernameAndPassword(String username, String password) {
+        EntityManager em = getNewNetityManager();
+        try {
+            TypedQuery<User> query = em.createNamedQuery("User.byUsernameAndPassword", User.class);
+            query.setParameter("username", username);
+            query.setParameter("password", password);
+            return query.getSingleResult();
+        } catch (NoResultException ex) {
+            return null;
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        } finally {
+            em.close();
+        }
+    }
+
+    public Collection<Trip> getUserTrips(User user) {
+        EntityManager em = getNewNetityManager();
+        try {
+            Collection<Trip> trips = em.find(User.class, user.getId()).getTrips();
+            Hibernate.initialize(trips);
+            return trips;
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        } finally {
+            em.close();
+        }
+    }
+
+    public Trip getUserTrip(int tripId, int userId) {
+        if (tripId != 0 && userId != 0) {
+            EntityManager em = getNewNetityManager();
+            try {
+                TypedQuery<Trip> query = em.createNamedQuery("Trip.getByIdIfCorrectUser", Trip.class);
+                query.setParameter("tripId", tripId);
+                query.setParameter("userId", userId);
+                return query.getSingleResult();
+            } catch (NoResultException ex) {
+                return null;
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            } finally {
+                em.close();
+            }
+        }
+        return null;
+    }
+
+    public Trip addOrUpdateTrip(Trip trip) {
+        if (trip != null) {
+            EntityManager em = getNewNetityManager();
+            try {
+                em.getTransaction().begin();
+                Trip merged = em.merge(trip);
+                em.getTransaction().commit();
+                return merged;
+            } catch (NoResultException ex) {
+                return null;
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            } finally {
+                em.close();
+            }
+        }
+        return null;
+    }
+
+    public boolean deleteTrip(int tripId, int userId) {
+        if (tripId != 0 && userId != 0) {
+            EntityManager em = getNewNetityManager();
+            try {
+                em.getTransaction().begin();
+                Query query = em.createNamedQuery("Trip.removeByIdIfCorrectUser");
+                query.setParameter("tripId", tripId);
+                query.setParameter("userId", userId);
+                int result = query.executeUpdate();
+                em.getTransaction().commit();
+                return result == 1;
+            } catch (Exception ex) {
+                return false;
+            } finally {
+                em.close();
+            }
+        }
+        return false;
+    }
+
+    public List<Geopoint> getTripGeopoints(int tripId, int userId) {
+        if (tripId != 0 && userId != 0) {
+            EntityManager em = getNewNetityManager();
+            try {
+                Query query = em.createNamedQuery("Geopoint.getListByTripIdIfUser");
+                query.setParameter("tripId", tripId);
+                query.setParameter("userId", userId);
+                //noinspection unchecked
+                return (List<Geopoint>) query.getResultList();
+            } catch (NoResultException ex) {
+                return null;
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            } finally {
+                em.close();
+            }
+        }
+        return null;
+    }
+
+    public Geopoint addOrUpdateGeopoint(Geopoint geopoint) {
+        if (geopoint != null) {
+            EntityManager em = getNewNetityManager();
+            try {
+                em.getTransaction().begin();
+                Geopoint merged = em.merge(geopoint);
+                em.getTransaction().commit();
+                return merged;
+            } catch (NoResultException ex) {
+                return null;
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            } finally {
+                em.close();
+            }
+        }
+        return null;
     }
 }
