014148fa0a7c7576b62c1ee586fa50b31584f928
Massimiliano Cristarella <m.cristarella@outlook.com>
2022-09-06 00:37:38 +0200

    Check if session is not expired inside AuthFilter

--- src/main/java/com/mcris/triprecorder/filters/AuthFilter.java
+++ src/main/java/com/mcris/triprecorder/filters/AuthFilter.java
@@ -7,8 +7,8 @@ import com.mcris.triprecorder.providers.DBProvider;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.Response;
-import java.io.IOException;
-import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.Instant;
 
 public class AuthFilter implements ContainerRequestFilter {
     // https://dennis-xlc.gitbooks.io/restful-java-with-jax-rs-2-0-2rd-edition/content/en/part1/chapter15/programmatic_security.html
@@ -31,6 +31,14 @@ public class AuthFilter implements ContainerRequestFilter {
         String token = parts[1];
         Session session = DBProvider.getInstance().getSession(token);
         if (session == null || session.getUser() == null) {
+            containerRequestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
+            return;
+        }
+        // TODO: check time zone?
+        Instant now = Instant.now();
+        Timestamp tsNow = Timestamp.from(now);
+        if (tsNow.after(session.getExpireAt())) {
+            DBProvider.getInstance().deleteSession(session.getToken());
             containerRequestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
             return;
         }
