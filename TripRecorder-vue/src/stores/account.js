65b159d20fa9e27fd2c331bb50ad6120819a1ede
Massimiliano Cristarella <massijay@outlook.com>
2022-09-07 18:04:12 +0200

    Add user account methods inside accountStore, add NavBar and get User data on loading MainView

--- TripRecorder-vue/src/stores/account.js
+++ TripRecorder-vue/src/stores/account.js
@@ -1,11 +1,31 @@
 import {defineStore} from 'pinia';
 import {computed, ref} from 'vue';
 import axios from 'axios';
+import router from "../router";
 
 export const useAccountStore = defineStore('account', () => {
     const apiKey = ref(null);
+    const user = ref(null);
+    let axiosInterceptor = null;
 
-    const isAuthenticated = computed(() => apiKey.value !== null);
+    const isAuthenticated = computed(() => {
+        if (apiKey.value !== null) {
+            return true;
+        }
+        const api = localStorage.getItem('apiKey');
+        if (api !== null) {
+            apiKey.value = api;
+            setupAxios();
+            return true;
+        }
+        return false;
+    });
+
+    function getUserData() {
+        return axios.get(import.meta.env.VITE_API_URL + '/profile')
+            .then(response => user.value = response.data)
+            .catch(err => console.log("Impossibile caricare i dati dell'utente", err));
+    }
 
     function login(username, password) {
         return axios.post(import.meta.env.VITE_API_URL + '/auth/login',
@@ -15,9 +35,61 @@ export const useAccountStore = defineStore('account', () => {
             })
             .then(response => {
                 apiKey.value = response.data['token'];
+                setupAxios();
+                localStorage.setItem("apiKey", apiKey.value);
             });
     }
 
-    return {apiKey, isAuthenticated, login};
+    function logout() {
+        return axios.post(import.meta.env.VITE_API_URL + '/auth/logout', {})
+            .then(response => {
+                clearSessionData();
+                router.push({name: 'login'}).then(() => {
+                });
+            })
+            .catch(err => {
+                clearSessionData()
+                console.log("Impossibile contattare il server", err)
+                router.push({name: 'login'}).then(() => {
+                });
+            });
+    }
+
+    function clearSessionData() {
+        apiKey.value = null;
+        localStorage.removeItem('apiKey');
+        resetAxios();
+    }
+
+    function setupAxios() {
+        axios.defaults.headers.common['Authorization'] = `Bearer ${apiKey.value}`;
+        if (axiosInterceptor === null) {
+            axiosInterceptor = axios.interceptors.response.use(function (response) {
+                // Any status code that lie within the range of 2xx cause this function to trigger
+                // Do something with response data
+                return response;
+            }, function (error) {
+                if (error.response.status === 401) {
+                    clearSessionData();
+                    router.push({name: 'login'}).then(() => {
+                    });
+                    //https://github.com/axios/axios/issues/583#issuecomment-504317347
+                    return new Promise(() => {
+                    });
+                }
+                return Promise.reject(error);
+            });
+        }
+    }
+
+    function resetAxios() {
+        delete axios.defaults.headers.common['Authorization'];
+        if (axiosInterceptor !== null) {
+            axios.interceptors.response.eject(axiosInterceptor);
+            axiosInterceptor = null;
+        }
+    }
+
+    return {isAuthenticated, user, getUserData, login, logout};
 
 })
--- TripRecorder-vue/src/views/MainView.vue
+++ TripRecorder-vue/src/views/MainView.vue
@@ -1,18 +1,45 @@
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125"/>
-
-    <div class="wrapper">
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/login">Login</RouterLink>
-      </nav>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <RouterLink class="navbar-brand" to="/">✈️ Trip Recorder</RouterLink>
+      <!--      <a class="navbar-brand" href="#">Navbar w/ text</a>-->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+              aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <RouterLink class="nav-link active" aria-current="page" to="/">Home</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/login">Login</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/">Pricing</RouterLink>
+          </li>
+        </ul>
+        <span class="navbar-text">
+          Ciao {{ accountStore.user?.username }}
+      </span>
+        <ul class="navbar-nav mb-2 mb-lg-0">
+          <li class="nav-item">
+            <button class="nav-link" @click="accountStore.logout()">Logout</button>
+          </li>
+        </ul>
+      </div>
     </div>
-  </header>
-  <RouterView />
+  </nav>
+  <RouterView/>
 </template>
 
-<script>
+<script setup>
+import {onBeforeMount} from 'vue'
+import {useAccountStore} from "../stores/account";
+
+const accountStore = useAccountStore();
+onBeforeMount(() => {
+  accountStore.getUserData();
+});
 
 </script>
